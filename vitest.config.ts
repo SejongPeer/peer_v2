a5127b7a24cdde33f38657a930e6da8afe77d4c4
JunYoungKr <kop981020@naver.com>
2024-08-22 10:23:57 +0900

    test8

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    include: ["tests/**/*.test.ts", "tests/**/*.spec.ts"], // 테스트 파일 경로 수정
+    include: ["src/**/*.test.ts", "src/**/*.spec.ts"], // 테스트 파일 경로 수정
     exclude: ["node_modules", "dist"],
   },
 });
