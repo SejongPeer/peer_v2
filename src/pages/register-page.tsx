9a0878e73993d1de497f5cdf5244ea26b7f6d7f7
JunYoungKr <kop981020@naver.com>
2024-09-08 17:03:13 +0900

    임시 push

--- src/pages/register-page.tsx
+++ src/pages/register-page.tsx
@@ -1,4 +1,4 @@
-import { useSearchParams, useNavigate } from "react-router-dom";
+import { useSearchParams } from "react-router-dom";
 import { RegisterStep1 } from "../components/register/registerStep1";
 import { RegisterStep2 } from "../components/register/registerStep2";
 import { RegisterStep3 } from "../components/register/registerStep3";
@@ -6,8 +6,8 @@ import { RegisterStep4 } from "../components/register/registerStep4";
 import { SubHeader } from "../components/subHeader";
 
 export const RegisterPage = () => {
-  const [searchParams, setSearchParams] = useSearchParams();
-  const navigate = useNavigate();
+  const [searchParams] = useSearchParams();
+  // const navigate = useNavigate();
 
   // 현재 step을 가져옴, 기본값은 1
   const step = searchParams.get("step") || "1";
